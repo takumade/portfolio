d82e1ec03c4aa34e274b321682a28c86a1172833
Takunda Madechangu <madechangu.takunda@gmail.com>
2021-12-26 19:10:52 +0200

    Added constants

---
+++ constants/data.ts
@@ -0,0 +1,169 @@
+const userData = {
+  githubUsername: "takumade",
+  name: "Takunda Madechangu",
+  designation: "Software Developer",
+  avatarUrl: "/avatar.webp",
+  email: "hello@taku.co.zw",
+  phone: "+263 77 854 8832",
+  address: "Harare, Zimbabwe.",
+  projects: [
+    {
+      title: "Bits Solutions",
+      link: "https://bitssolutions.co.zw",
+      imgUrl: "/projects/bits.webp",
+      stack: ["React", "NextJS", "TailwindCSS", "Laravel"],
+      description: "Bitssolutions is a software company that provides solutions to businesses.",
+      platform: "Web",
+      status: "Done",
+    },
+    {
+      title: "Mutenga Bamboo",
+      link: "https://mutengabamboo.co.zw",
+      imgUrl: "/projects/bamboo.webp",
+      stack: ["React", "NextJS", "TailwindCSS", "Laravel"],
+      description: "Mutenga Bamboo is a company involved in bamboo and its products.",
+      platform: "Web",
+      status: "Done",
+    },
+    {
+      title: "Trinity",
+      link: "https://mutengabamboo.co.zw",
+      imgUrl: "/projects/vimeo.webp",
+      stack: ["Flutter", "Dart"],
+      description: "Vimeo Player Trinity is a Flutter package for playing any videos from Vimeo by id.",
+      platform: "Mobile",
+      status: "Done",
+    },
+    {
+      title: "Laratron",
+      link: "https://github.com/takumade/laratron",
+      imgUrl: "/projects/laratron.webp",
+      stack: ["Electron", "Laravel"],
+      description: "Making Laravel desktop application using Electron Js(For MAC OS).",
+      platform: "Desktop",
+      status: "Done",
+    },
+    {
+      title: "Taskmaster",
+      link: "https://github.com/takumade/taskmaster",
+      imgUrl: "/projects/taskmaster.webp",
+      stack: ["Electron", "React", "TailwindCSS", "Recoil"],
+      description: "Offline first kanban app. It allows creation and grouping of boards and tasks.",
+      platform: "Desktop",
+      status: "Development",
+    },
+    {
+      title: "Chazos",
+      link: "https://github.com/Chazos/chazos",
+      imgUrl: "/projects/chazos.webp",
+      stack: ["Laravel",  "TailwindCSS", "AlpineJS"],
+      description: "Is Headless CMS for Laravel. It eases content creation and consuming.",
+      platform: "Web",
+      status: "Development",
+    },
+
+    
+  
+  ],
+  about: {
+    title:
+      "I'm a Zimbabwe-based software developer who specializes in building (and occasionally designing) exceptional digital experiences.",
+    description: [
+      `My interest in development started back in 2017 when I decided to self-teach myself Python — I would also experiment with codes that i find on the web inorder to understand how they work.`,
+      `My main focus these days is building accessible, inclusive products and digital experiences.`,
+    ],
+    currentProject: "Taskmaster",
+    currentProjectUrl: "https://github.com/takumade/taskmaster",
+  },
+  experience: [
+    {
+      title: "Web Developer",
+      company: "Mutenga Bamboo, Harare",
+      year: "2021 -> Present",
+      companyLink: "https://mutengabamboo.co.zw",
+      desc: "Design, develop, test, deploy and maintain Mutenga Bamboo website",
+    },
+    {
+      title: "Web Developer",
+      company: "Bitssolutions, Harare.",
+      year: "2021 -> Present",
+      companyLink: "https://bitssolutions.co.zw",
+      desc: "Design, develop, test, deploy and maintain Bitssolutions website.",
+    },
+
+    {
+      title: "Software Developer",
+      company: "263 Africa Media, Harare",
+      year: "2020 -> Present",
+      companyLink: "https://playafrika.tv",
+      desc: "Design, develop, test, deploy and maintain web apps, mobile apps and APIs.",
+    },
+    
+    {
+      title: "Frontend Engineer",
+      company: "Omni Learning, Harare",
+      year: "2020 -> 2021",
+      companyLink: "https://www.linkedin.com/company/omni-learning",
+      desc: "Transform designs into frontent code. Worked closely with designers, marketing and management team to develop, document, and manage an Omni Project",
+    },
+    {
+      title: "Software Development Intern",
+      company: "263Africa TV, Harare.",
+      year: "2019 -> 2019",
+      companyLink: "https://www.linkedin.com/company/263africatv",
+      desc: "Design, develop, test, deploy and mantain web projects.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1RDZCNOomkdo6IwEVkj7DC91aPgDD_Kk-/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/madeit44",
+    twitter: "https://twitter.com/takucoder",
+    linkedin: "https://www.linkedin.com/in/tmadechangu/",
+    github: "https://github.com/takumade",
+    facebook: "https://facebook.com/",
+  },
+  stacks: {
+    frontend: [
+      "ReactJS",
+      "TailwindCSS",
+      "Bootstrap",
+      "JQueryUI",
+      "JS"
+    ],
+    backend: [
+      "NodeJS",
+      "Express",
+      "Laravel",
+      "Django",
+      "NextJS"
+    ],
+    database: [
+      "MongoDB",
+      "MySQL",
+      "Firebase",
+      "SQLite"
+    ],
+    mobile: [
+      "Flutter",
+      "Electron"
+    ],
+    langauges: [
+      "Python",
+      "PHP",
+      "Javascript",
+      "Typescript",
+      "Dart",
+    ],
+    tools: [
+      "Git",
+      "GitHub",
+      "ClickUp",
+      "Trello",
+      "Slack",
+      "Cpanel"
+    ]
+  }
+};
+
+export default userData;
